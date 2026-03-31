f0cd50bd5986cfd592d0c6920f4715b5926c45b9
jhbaek <jhbeak@wavus.co.kr>
2026-03-31 16:48:06 +0900

    주석 삭제

--- src/test/java/com/jonanlog/api/controller/PostControllerTest.java
+++ src/test/java/com/jonanlog/api/controller/PostControllerTest.java
@@ -7,7 +7,6 @@ import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
