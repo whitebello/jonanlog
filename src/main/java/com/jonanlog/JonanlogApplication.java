6e4858040671c664d9bc2fbd287e7ff18e835c37
jhbaek <jhbeak@wavus.co.kr>
2026-03-31 11:23:09 +0900

    jonanlog 최초커밋

---
+++ src/main/java/com/jonanlog/JonanlogApplication.java
@@ -0,0 +1,13 @@
+package com.jonanlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JonanlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JonanlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jonanlog/JonanlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jonanlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JonanlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
